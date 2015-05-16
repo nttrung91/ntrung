afe56f5d4eaac198ada37eb7adbc77ea2b56ef57
Kubi <trungn3@uci.edu>
2015-05-15 20:00:37 -0700

    Cleaned up JS Code

--- js/build/production.js
+++ js/build/production.js
@@ -1408,52 +1408,59 @@ $(document).on('click','.js-nav-toggle', function(e) {
   $('.js-nav-panel').toggleClass('is-hidden');
   e.preventDefault();
 });
-var trung = trung || {};
-
-trung.plugIns = {
-
-  init: function() {
-    trung.plugIns.fastClick();
-    trung.plugIns.styleCurrentPage();
-  },
-
-  fastClick: function() {
-    FastClick.attach(document.body);
-  },
-
-  // Add 'is-active' class to selected page on Menu
-  styleCurrentPage: function() {
-    $('a[href^="/' + location.pathname.split("/")[1] + '"]').addClass('is-active');
-  }
-};
-
-
-
-
-
-// INITS
-trung.plugIns.init();
-
-
-
-
-// SmoothState can't be added to trung.plugIns
 ;(function($) {
   'use strict';
-  var $body = $('html, body'),
-      content = $('#main').smoothState({
-        prefetch: true,
-        // Runs when a link has been activated
-        onStart: {
-          duration: 250,
-          render: function (url, $container) {
-            _gaq.push(['_trackPageview', url]);
-            content.toggleAnimationClass('is-exiting');
-            $body.animate({
-              scrollTop: 0
-            });
-          }
-        }
-      }).data('smoothState');
-      //.data('smoothState') makes public methods available
+
+  var trung = trung || {};
+
+  trung.menu = {
+
+    init: function() {
+      this.highlightCurrentMenu();
+    },
+
+    // Add 'is-active' class to selected page on Menu
+    highlightCurrentMenu: function() {
+      $('a[href^="/' + location.pathname.split("/")[1] + '"]').addClass('is-active');
+    }
+  };
+
+
+  trung.plugIns = {
+
+    init: function() {
+      this.fastClick();
+      this.smoothState();
+    },
+
+    fastClick: function() {
+      FastClick.attach(document.body);
+    },
+
+    smoothState: function() {
+      var $body = $('html, body'),
+          content = $('#main').smoothState({
+            prefetch: true,
+            // Runs when a link has been activated
+            onStart: {
+              duration: 250,
+              render: function (url, $container) {
+                _gaq.push(['_trackPageview', url]);
+                content.toggleAnimationClass('is-exiting');
+                $body.animate({
+                  scrollTop: 0
+                });
+              }
+            }
+          }).data('smoothState');
+    }
+  };
+
+
+
+
+
+  // INITS
+  trung.menu.init();
+  trung.plugIns.init();
 })(jQuery);
